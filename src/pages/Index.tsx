83f9120932365cc07223c0aa2033dfb57c74878e
Muneeb Ahmed Khan <muneebkhan.mk2018@gmail.com>
2025-06-25 19:11:39 +0500

    new

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -60,7 +60,7 @@ const Index = () => {
             <Hero />
             <div className="bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
               <div className="container mx-auto px-4 py-16 space-y-16">
-                <TaskManager />
+                {/* <TaskManager /> */}
                 {/* <Dashboard /> */}
               </div>
             </div>
